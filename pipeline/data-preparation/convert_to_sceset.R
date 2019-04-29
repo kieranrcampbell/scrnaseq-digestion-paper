fd7b96bbc7d011e3de54d8208c067c1e325a728b
kieranrcampbell <kieranrcampbell@gmail.com>
2019-04-29 15:10:33 -0700

    Move to data-preparation directory for parsing input

---
+++ pipeline/data-preparation/convert_to_sceset.R
@@ -0,0 +1,103 @@
+
+suppressPackageStartupMessages({
+  library(SingleCellExperiment)
+  library(aargh)
+  library(scater)
+  library(jsonlite)
+  library(DropletUtils)
+})
+
+#' Work out whether we're dealing with V2 or V3
+#'
+workout_v2v3 <- function(path) {
+  files <- dir(path)
+  
+  if("genes.tsv" %in% files) {
+    return(2)
+  } 
+  if("features.tsv" %in% files) {
+    return(3)
+  }
+  stop("Input not recognized as V2 or V3")
+}
+
+
+#' Read 10X accounting for V2 and V3 differences
+read_10X <- function(path) {
+  version <- workout_v2v3(path)
+  
+  if(version == 2) {
+    return(read10xCounts(path))
+  }
+  
+  if(version == 3) {
+    d <- tempdir()
+    files <- dir(path, full.names = TRUE)
+    for(f in files) {
+      file.copy(f, d)
+    }
+    file.rename(file.path(d, "features.tsv"), file.path(d, "genes.tsv"))
+    
+    sce <- read10xCounts(d)
+    rowData(sce)[,3] <- NULL
+    return(sce)
+  }
+  
+  stop("Only V2 and V3 currently supported")
+}
+
+
+read_sce <- function(input_sce = "input",
+                     output_sce = "output",
+                     metadata_json = "str") {
+  
+
+  sce <- readRDS(input_sce)
+  
+
+  mdata <- fromJSON(metadata_json)
+  
+  for(m in sort(names(mdata))) {
+    colData(sce)[[ m ]] <- mdata[[ m ]]
+  }
+  
+
+  rowData(sce)$ensembl_gene_id <- rownames(sce)
+  
+  sce <- getBMFeatureAnnos(sce, filters = "ensembl_gene_id",
+  attributes = c("ensembl_gene_id", "hgnc_symbol", "entrezgene",
+  "start_position", "end_position", "chromosome_name"),
+  dataset = "hsapiens_gene_ensembl")
+
+  
+  # Calculate size factors
+  sce <- scran::computeSumFactors(sce)
+  
+  
+  # Compute log normal expression values
+  sce <- normalize(sce)
+  
+  # Compute doublet scores
+  sce$doublet_score <- scran::doubletCells(sce)
+  
+  # Get Mitochondrial genes for QC:
+  mt_genes <- which(rowData(sce)$chromosome_name == "MT")
+  ribo_genes <- grepl("^RP[LS]", rowData(sce)$Symbol)
+  feature_ctrls <- list(mito = rownames(sce)[mt_genes],
+                        ribo = rownames(sce)[ribo_genes])
+  
+  # Calculate QC metrics
+  sce <- calculateQCMetrics(sce, feature_controls = feature_ctrls)
+  
+
+  
+  # Make sure colnames are unique
+  colnames(sce) <- paste0(metadata(sce)$id, "_", sce$Barcode)
+  
+  # Save to output
+  saveRDS(sce, output_sce)
+}
+
+aargh(read_sce)
+
+
---
+++ pipeline/data-preparation/data-preparation.smk
@@ -0,0 +1,73 @@
+import pandas as pd
+import os
+import json
+
+
+configfile: "private_config.yaml"
+
+df_inventory = pd.read_csv(config['sample_inventory_url']).dropna()
+
+assert df_inventory.shape[0] == 45
+
+df_inventory.index = df_inventory['id']
+inventory_dict = df_inventory.to_dict('index')
+
+ids = list(inventory_dict.keys())
+
+cellranger_versions = ['v3']
+
+# SingleCellExperiments
+sces_raw = expand("data/scesets/{cv}/{id}_sceset_{cv}_raw.rds",
+                  id=ids, cv = cellranger_versions)
+sces_qc = expand("data/scesets/{cv}/{id}_sceset_{cv}_qc.rds",
+                  id=ids, cv = cellranger_versions)
+
+# TODO: move this to separate file
+mito_des = expand("data/mito_differential_expression/{id}_mito_de.csv",
+                  id = ids)
+
+
+
+rule parse_nick_output:
+    params:
+        metadata_json=lambda wildcards: json.dumps(inventory_dict[wildcards.id])
+    input:
+        "data/temperature_rdata_v3/{id}.rdata" # <- file from nick's pipeline
+    output:
+        "data/scesets/v3/{id}_sceset_v3_raw.rds"
+    shell:
+        "Rscript pipeline/data-preparation/convert_to_sceset.R \
+        --input_sce {input} \
+        --output_sce {output} \
+        --metadata_json '{params.metadata_json}'"
+        
+
+rule qc_scesets:
+    params:
+        curr_dir = os.getcwd()
+    input:
+        "data/scesets/{cv}/{id}_sceset_{cv}_raw.rds"
+    output:
+        sce="data/scesets/{cv}/{id}_sceset_{cv}_qc.rds",
+        report="reports/qc/{cv}/qc_report_{id}_{cv}.html"
+    shell:
+        "Rscript -e \"rmarkdown::render('pipeline/qc/sce_qc.Rmd', \
+        output_file='{params.curr_dir}/{output.report}', knit_root_dir='{params.curr_dir}', \
+        params=list(input_sce_path='{input}', output_sce_path='{output.sce}'))\" "
+
+rule mito_de:
+    input:
+        "data/scesets/{id}_sceset_raw.rds"
+    output:
+        "data/mito_differential_expression/{id}_mito_de.csv"
+    shell:
+        "Rscript pipeline/all_sample_mito/all_sample_mito_de.R --sce_input_file {input} --output_csv {output}"
+    
+
+
+    
+
+
+
+
+
